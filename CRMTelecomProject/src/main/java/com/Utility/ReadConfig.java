8d36b3aebaea075a96e57c82c86e247dce69db5d
Saroj <sarojpulajwar4444@gmail.com>
2022-12-26 17:40:34 +0530

    class property

--- CRMTelecomProject/src/main/java/com/Utility/ReadConfig.java
+++ CRMTelecomProject/src/main/java/com/Utility/ReadConfig.java
@@ -2,21 +2,25 @@ package com.Utility;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 import com.TestBase.TestBaseClass;
 
 public class ReadConfig extends TestBaseClass
 {
 	
-	public ReadConfig() throws IOException 
+	public ReadConfig()  
 	{
 		String configpath=System.getProperty("user.dir")+"\\Config\\Config.properties";
-		//String configpath="C:\\Eclipse Framework\\Suite_CRM_Telecom_Project\\CRMTelecomProject\\Config\\Config.properties";
 		File file = new File(configpath);
-	    FileInputStream fis = new FileInputStream(file);
-	    property.load(fis);
+	    try 
+	    {
+		    FileInputStream fis = new FileInputStream(file);
+		    property.load(fis);
+
+	    }
+	    catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
 	}
 	
 	public String getApplicationUrl() 
